7691ad7f5170db53e058753bb8ccff210b46a34c
Neil Northrop <nnorthrop@gmail.com>
2013-06-07 16:56:38 -0700

    random board game generator

--- hello.rb
+++ hello.rb
@@ -1,11 +1,42 @@
-$stdout.puts "Goodbye, World!"
-num = (1..26)
-num_a = num.to_a
-puts num_a
+require 'pp'
+=begin
+array of board games
+rand generator
+=end
+class RandomGameGenerator
 
-real_life {
-	'neil' => 'bike'
-	'lemon' => 'cat'
-	'lulu' => 'cat'
-	'marty' => 'dog'
-}
+	def initialize
+		file_board_games = File.read('list_of_board_games.txt').split(',')# { |word| word.split(',')}
+
+		array_of_games = file_board_games.map do |word|
+			word.gsub(/^'|'$/, '')
+		end
+
+	end
+
+	def add_game
+		puts "Do you have a new game to put in?"
+		puts "Type 'yes' or 'no'"
+		response = gets.chomp.downcase
+			if response == 'yes'
+				puts "Enter new board game: "
+				new_game = gets.chomp
+				list << new_game
+			elsif response == 'no'
+					add_game
+			else
+				puts "Type 'yes' or 'no'"
+				return true
+			end
+	end
+
+end
+
+RandomGameGenerator.add_game
+# puts
+# number_in_list = f.length
+# puts number_in_list
+
+# x = rand(number_in_list)
+
+# puts x
