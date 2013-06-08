dad5a0c1e3c4cd6994cbd2aa3bbc4d63e7c90044
Neil Northrop <nnorthrop@gmail.com>
2013-06-08 13:54:16 -0700

    Added a random generator to array of games

--- hello.rb
+++ hello.rb
@@ -3,40 +3,37 @@ require 'pp'
 array of board games
 rand generator
 =end
-class RandomGameGenerator
-
-	def initialize
 		file_board_games = File.read('list_of_board_games.txt').split(',')# { |word| word.split(',')}
 
 		array_of_games = file_board_games.map do |word|
 			word.gsub(/^'|'$/, '')
 		end
 
-	end
+		# puts "Do you have a new game to put in?"
+		# puts "Type 'yes' or 'no'"
+		# response = gets.chomp.downcase
+		# 	if response == 'yes'
+		# 		puts "Enter new board game: "
+		# 		new_game = gets.chomp
+		# 		list << new_game
+		# 	elsif response == 'no'
+		# 			add_game
+		# 	else
+		# 		puts "Type 'yes' or 'no'"
+		# 		return true
+		# 	end
+puts
+number_in_list = array_of_games.length
+puts number_in_list
 
-	def add_game
-		puts "Do you have a new game to put in?"
-		puts "Type 'yes' or 'no'"
-		response = gets.chomp.downcase
-			if response == 'yes'
-				puts "Enter new board game: "
-				new_game = gets.chomp
-				list << new_game
-			elsif response == 'no'
-					add_game
-			else
-				puts "Type 'yes' or 'no'"
-				return true
-			end
-	end
+rand_game = rand(number_in_list)
 
+puts rand_game
+
+array_of_games.each_index do |array_index|
+	if array_index == rand_game
+		print array_of_games[rand_game]
+	end
 end
 
-RandomGameGenerator.add_game
-# puts
-# number_in_list = f.length
-# puts number_in_list
 
-# x = rand(number_in_list)
-
-# puts x
